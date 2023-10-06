3b1f52d3edaefe81607fc00d9b0c12c4b7b17037
linyark <he_fengchen@qq.com>
2023-10-06 18:55:21 +0800

    add draw img.

--- light_trace/1_Parallel_light_in_the_meridian_plane/auto.py
+++ light_trace/1_Parallel_light_in_the_meridian_plane/auto.py
@@ -1,46 +1,120 @@
-import os 
+import os
 import sys
+
 sys.path.append(os.getcwd())
+sys.dont_write_bytecode = True
 
-import math
+import numpy as np
+import matplotlib.pyplot as plt
 
-from tracelib.surface import surface_lib
+from light_trace.tracelib.common import *
+
+from light_trace.tracelib.surface import surface_lib
+
 surfaces = [
-    surface_lib(r=100, h=50, t=50, n=1.5168),
-    surface_lib(r='inf',h=50, t=160, n=1),
-    surface_lib(r='inf',h=50, t='inf', n=1)
+    surface_lib(r=100, h=50, t=50, n=1.1168),
+    surface_lib(r=100, h=50, t=60, n=1.9),
+    surface_lib(r="inf", h=50, t=160, n=1),
+    surface_lib(r="inf", h=50, t="inf", n=1),
 ]
 
-from tracelib.light import light_lib
+from light_trace.tracelib.light import light_lib
+
 lights = [
-    light_lib(q=10, u=0),
-    light_lib(q=0,  u=0),
-    light_lib(q=-10,u=0),
+    light_lib(q=10, u=0, p=0),
+    light_lib(q=0, u=0, p=0),
+    light_lib(q=-10, u=0, p=0),
 ]
 
+# trace
 last_n, last_t, sum_t = 1, 0, 0
-last_lights = lights
-for i,surface in enumerate(surfaces):
-    c, r, t, n_1 = 1/surface.r, surface.r, surface.t, surface.n
+all_lights = []
+all_lights.append(lights)
+for i, surface in enumerate(surfaces):
+    c, r, t, n_1 = 1 / surface.r, surface.r, surface.t, surface.n
     out_lights = []
-    for light in last_lights:
+    for light in all_lights[i]:
         u = light.u
-        q = light.q + math.sin(u)*last_t
-        sinI = q*c + math.sin(u)
+        q = light.q + sin(u) * last_t
+        sinI = q * c + sin(u)
         sinI_1 = last_n * sinI / n_1
-        u_1 = u - math.asin(sinI) + math.asin(sinI_1)
-        q_1 = r * (sinI_1 - math.sin(u_1))
-        out_lights.append(light_lib(q=q_1, u=u_1, p=sum_t, idx=i+1))
-    if t != 'inf':
+        u_1 = u - asin(sinI) + asin(sinI_1)
+        q_1 = r * (sinI_1 - sin(u_1))
+        out_lights.append(light_lib(q=q_1, u=u_1, p=sum_t))
+    if t != "inf":
         sum_t = sum_t + t
-
     last_n, last_t = surface.n, surface.t
-    last_lights = out_lights
-    for l in out_lights:
-        print(f"[p {l.p:6}], [q {l.q:8.3f}], [u {l.u:8.3f}], [idx {l.idx:4}]")
-    print("finish one surface")
+    all_lights.append(out_lights)
 
 print("final check")
-for l in last_lights:
-    print(f"[p {l.p:6}], [q {l.q:8.3f}], [u {l.u:8.3f}]")
+for i, lights in enumerate(all_lights):
+    for l in lights:
+        print(f"[{i:2}, p q u], [{l.p:5} {l.q:8.3f} {l.u:8.3f}]")
+    print()
+
+
+# draw
+def get_cross_point(all_lights):
+    all_cross_points = []
+
+    input_lights = all_lights[0]
+    cross_points = []
+    for l in input_lights:
+        if l.u != 0:
+            z = 0
+            y = tan(l.u) * (z - l.p + l.q / (sin(l.u)))
+        else:
+            z = 0
+            y = l.q
+        cross_points.append([z, y])
+    all_cross_points.append(cross_points)
+
+    
+    for i in range(len(all_lights) - 2):
+        lights_in, lights_out = all_lights[i], all_lights[i + 1]
+        cross_points = []
+        for j in range(len(lights_in)):
+            l1, l2 = lights_in[j], lights_out[j]
+            if l1.u != 0 and l2.u != 0 and l1.u != l2.u:
+                z = (
+                    tan(l1.u) * (l1.p - l1.q / sin(l1.u))
+                    - tan(l2.u) * (l2.p - l2.q / sin(l2.u))
+                ) / (tan(l1.u) - tan(l2.u))
+                y = tan(l2.u) * (z - l2.p + l2.q / sin(l2.u))
+            elif l1.u == 0 and l2.u != 0:
+                z = l1.q / tan(l2.u) + l2.p - l2.q / sin(l2.u)
+                y = l1.q
+            elif l1.u != 0 and l2.u == 0:
+                z = l2.q / tan(l1.u) + l1.p - l1.q / sin(l1.u)
+                y = l2.q
+            elif l1.u == 0 and l2.u == 0:
+                z = l2.p
+                y = l2.q
+            else:
+                z = l2.p
+                y = tan(l2.u) * (z - l2.p + l2.q / sin(l2.u))
+            cross_points.append([z, y])
+        all_cross_points.append(cross_points)
+
+    input_lights = all_lights[-1]
+    cross_points = []
+    for l in input_lights:
+        if l.u != 0:
+            z = l.p
+            y = tan(l.u) * (z - l.p + l.q / (sin(l.u)))
+        else:
+            z = l.p
+            y = l.q
+        cross_points.append([z, y])
+    all_cross_points.append(cross_points)
+
+    return all_cross_points
+
+all_cross_points = get_cross_point(all_lights)
+points = np.array(all_cross_points)
+_,num_lights,_ = points.shape
+for i in range(num_lights):
+    plt.plot(points[:,i,0],points[:,i,1]) #color='b',linestyle = 'dotted',
+plt.show()
+
 a = 1
--- light_trace/tracelib/light.py
+++ light_trace/tracelib/light.py
@@ -2,13 +2,11 @@ class light_lib():
     def __init__(self,
                  q = 10,
                  u = 0,
-                 p = 0,
-                 idx = 0
+                 p = 0
                 ):
         self.q = q
         self.u = u
         self.p = p
-        self.idx = idx
 
 if __name__=="__main__":
     l = light_lib()
