a0bce607a0e922b40a8639081accea66cdec1f58
linyark <he_fengchen@qq.com>
2023-11-23 17:45:18 +0800

    gen dev 0.0.2311231745

--- ray_gen/ray_tracing/connet/connet_normal.py
+++ ray_gen/ray_tracing/connet/connet_normal.py
@@ -1,23 +1,27 @@
 import torch
 
-config = {}
-config["surfaces"] = [
-    (50, 50, [True, False, False], 1.5168, 40),
-    (-100, 60, [False, False, False], 2.02204, 40),
-    (torch.inf, 100, [False, False, False], 1, 40),
-    (100, 50, [False, False, False], 1.5168, 40),
-    (-100, 60, [False, False, False], 1, 40),
-    (torch.inf, torch.inf, [False, False, False], 1, 40),
-]
-
 
 class Normal:
     def __init__(self) -> None:
         pass
 
+    def g_thick_map(self, input):
+        out = (input * 10) + 15  # 5-25
+        return out
+
+    def a_thick_map(self, input):
+        out = (input * 50) + 60  # 10-110
+        return out
+
     def connect(self, input):
-        config["surfaces"] = [
-            (50, 50, [True, False, False], 1.5168, 40),
-            (-100, 60, [False, False, False], 2.02204, 40),
-            (torch.inf, torch.inf, [False, False, False], 1, 40),
-        ]
+        config_list = []
+        for i, bs in enumerate(input):
+            conbine = bs
+
+            config = [
+                (bs[0], self.g_thick_map(bs[1]), [False, False, False], 1.5168, 40),
+                (bs[2], self.a_thick_map(bs[3]), [False, False, False], 1, 40),
+                (torch.inf, torch.inf, [False, False, False], 1, 40),
+            ]
+            config_list.append(config)
+        return config_list
